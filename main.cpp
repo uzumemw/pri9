f29f9aa7c1a5c5bd4e88581afde37f9de68e0fa0
uzumemw <prib-211_349298@volsu.ru>
2022-12-20 00:36:20 +0300

    repaired

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
+#include "sdt.h"
 
+int main()
+{
+
+int a,b;
+
+cout << "  Введите целое число а:  "
+cin >> a;
+
+cout << "  Введите целое число b:  "
 cin >> b;
+
 cout << "  a + b = " << a + b;	
 
 cout << "\n  a - b = " << a - b;
 
-cout << "\n  a / b= " << a / b;
+cout << "\n  a / b = " << a / b;
 
 cout << "\n  a * b = " << a * b;
 
